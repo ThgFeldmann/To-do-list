fa66c0b8f0a4648c354dab4c650c4a2c1892523e
Thiago <thiagofeldmann06@gmail.com>
2024-08-19 15:22:10 -0300

    criação dos cards e aplicação das cores

--- src/containers/Lista-de-tarefas/index.tsx
+++ src/containers/Lista-de-tarefas/index.tsx
@@ -1,8 +1,44 @@
+import Tarefa from '../../components/Tarefa'
+import { Container } from './styles'
+
+const tarefas = [
+  {
+    titulo: 'Estudar TypeScript',
+    descricao: 'Ver a aula 3 da EBAC',
+    prioridade: 'importante',
+    status: 'pendente'
+  },
+  {
+    titulo: 'Pagar a conta de internet',
+    descricao: 'Baixar fatura no Gmail',
+    prioridade: 'urgente',
+    status: 'concluída'
+  },
+  {
+    titulo: 'Ir para a academia',
+    descricao: 'Fazer o treino B',
+    prioridade: 'importante',
+    status: 'pendente'
+  }
+]
+
 const ListaDeTarefas = () => {
   return (
-    <main>
+    <Container>
       <p>2 tarefas marcadas como: &quot;categoria&quot; e &quot;termo&quot;</p>
-    </main>
+      <ul>
+        {tarefas.map((t) => (
+          <li key={t.titulo}>
+            <Tarefa
+              descricao={t.descricao}
+              titulo={t.titulo}
+              status={t.status}
+              prioridade={t.prioridade}
+            />
+          </li>
+        ))}
+      </ul>
+    </Container>
   )
 }
 
