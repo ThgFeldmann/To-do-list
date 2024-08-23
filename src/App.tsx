521342ba6faa4766594b751aa3bf601c392a5006
Thiago <thiagofeldmann06@gmail.com>
2024-08-23 14:20:32 -0300

    conclusão do projeto

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,30 @@
-import ListaDeTarefas from './containers/Lista-de-tarefas'
-import BarraLateral from './containers/Barra-lateral'
+import { Provider } from 'react-redux'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+
 import EstiloGlobal, { Container } from './styles'
+import store from './store'
+import Home from './pages/Home'
+import Cadastro from './pages/Cadastro'
+
+const rotas = createBrowserRouter([
+  {
+    path: '/',
+    element: <Home />
+  },
+  {
+    path: '/novo',
+    element: <Cadastro />
+  }
+])
 
 function App() {
   return (
-    <>
+    <Provider store={store}>
       <EstiloGlobal />
       <Container>
-        <BarraLateral />
-        <ListaDeTarefas />
+        <RouterProvider router={rotas} />
       </Container>
-    </>
+    </Provider>
   )
 }
 
